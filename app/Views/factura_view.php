ee1fb439e520879c909f02a69e829ce069094267
lauracosorio <laurisosorio7@hotmail.com>
2021-06-18 21:27:47 -0500

    boton pagar

--- app/Views/factura_view.php
+++ app/Views/factura_view.php
@@ -172,7 +172,7 @@
 
             <!-- Button trigger modal -->
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
+                Pagar
             </button>
 
             <!-- Modal -->
